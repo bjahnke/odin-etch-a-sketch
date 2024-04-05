4c9c38656f668285684119341bb918733e9ff289
Brian Jahnke <bjahnke71@gmail.com>
2024-04-05 17:01:26 -0400

    mouseover event gives random color to box and reduces opacity until black

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+function rainbow(lightness) {
+    // 30 random hues with step of 12 degrees
+    var hue = Math.floor(Math.random() * 30) * 12;
+
+    return `hsl(${hue}, 90%, ${lightness}%)`;
+}
 
 function createBlockDiv(){
     let newDiv = document.createElement('div');
     newDiv.classList.add('block');
+    let lightness = 60;
+    let reductionAmt = 6 //10% of initial lightness 
     newDiv.addEventListener('mouseover', function()  {
-        this.classList.add('red-element');
+        this.style.backgroundColor = rainbow(lightness);
+        lightness = Math.max(lightness - reductionAmt, 0);
     })
     return newDiv;
 }
@@ -36,7 +45,19 @@ function createGrid(num) {
 
 const container = document.querySelector('.container');
 
-
-
 container.append(...createGrid(16));
 
+const resizeButton = document.querySelector('button.resize');
+resizeButton.addEventListener('click', function() {
+    let minGridSize = 1;
+    let maxGridSize = 100;
+    let input = prompt(`Enter a new grid size [${minGridSize}-${maxGridSize}]: `);
+    let newGridSize = parseInt(input, 10);
+
+    if (isNaN(newGridSize) || newGridSize < 1 || newGridSize > 100) {
+        alert(`${input} is not a valid input. Must be a number between ${minGridSize} and ${maxGridSize}.`)
+        return 
+    }
+
+    container.replaceChildren(...createGrid(newGridSize));
+})
